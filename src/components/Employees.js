9a5b38ca581c1cb856ed547a46a49b421c911205
PromilaLaha98 <officialpromila@gmail.com>
2023-04-19 20:43:25 +0530

    commit

---
+++ src/components/Employees.js
@@ -0,0 +1,24 @@
+const Employees = [
+    {
+        id:"01",
+        Name:"Virat Kohli",
+        Age:"33",
+    },
+    {
+        id:"02",
+        Name:"Ranbir Singh",
+        Age:"36",
+    },
+    {
+        id:"03",
+        Name:"Dipika Padukon",
+        Age:"35",
+    },
+    {
+        id:"04",
+        Name:"Shah Rukh Khan",
+        Age:"50",
+    },
+]
+
+export default Employees;
